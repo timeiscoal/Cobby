6397ad6405d7ee31724155cc3e57f1eeaf9f4c2b
timeiscoal <timeiscoal@gmail.com>
2022-10-19 21:05:12 +0900

    add Image models clss and views def

--- makgeolli/views.py
+++ makgeolli/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 ,redirect
 from django.http import HttpResponse
 from makgeolli.models import Makgeolli
 import easyocr
+from .models import Cobby
 
 # 221018 최해민 막걸리 리스트 페이지 함수 추가
 def makgeolli(request):
@@ -20,3 +21,13 @@ def cobby(request):
     for result in result_list:
         name += result[1]
     return HttpResponse(f'{name}')
+
+# 221019 최신욱 사용자 Image 저장.
+def Image(request):
+    if request.method == 'GET':
+        return render(request, 'post/index.html')
+    if request.method =='POST':
+        user_image = Cobby()
+        user_image.image = request.FILES.get('images', '')
+        user_image.save()
+        return redirect('makgeolli:detail')
