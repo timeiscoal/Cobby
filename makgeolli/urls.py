f243eef238fbfda0976e65e67d03110adb41bdee
timeiscoal <113073492+timeiscoal@users.noreply.github.com>
2022-10-20 12:35:28 +0900

    Revert "수정: 템플릿 수정"

--- makgeolli/urls.py
+++ makgeolli/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     # 221018 최해민 Makgeolli url 추가
     path('makgeolli/', views.makgeolli, name='makgeolli'),
     path('makgeolli/<int:id>', views.makgeolli_detail, name='detail'),
+<<<<<<< HEAD
+
+=======
+>>>>>>> 4c1142cd4267f9e35b932d510f887272e2b61095
+
 ]
 
