c838ae45291eb44665da0f1baa784706b89f05bc
timeiscoal <timeiscoal@gmail.com>
2022-10-20 11:45:05 +0900

    검색 기능 연결

--- post/urls.py
+++ post/urls.py
@@ -4,7 +4,7 @@ from post import views
 app_name = 'post'
 
 urlpatterns = [
-    path('post/', views.Post_search, name='post_search'),
+    path('makgeolli/<int:id>', views.Post_search, name='post_search'),
     path('comment/delete/<int:pk>/', views.comment_delete, name='delete'),
     path('comment/<int:id>/', views.create_comment, name= 'create_comment'), 
     path('edit/<int:id>/', views.edit, name = 'comment_edit'),
